4ec4533ee05834dbab590cf24294def048c63877
thewillft <wftjen@gmail.com>
2024-07-24 16:54:45 -0400

    add basic navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,10 @@
+
+
+
+export default function Navbar() {
+    return (
+        <nav className="py-3 px-5 bg-blue-400 shadow">
+            <h1 className="text-white text-lg">Secure Census System</h1>
+        </nav>
+    )
+}
